92937a088ea371a0ac449552f2903657f0544fbf
pamp24 <pamporakis.fotis@gmail.com>
2025-03-28 17:02:02 +0200

    First Commit

---
+++ tests/workingWithAPI.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+import tags from '../tests/test-data/tags.json';
+
+test.beforeEach(async({ page }) => {
+  await page.route('*/**/api/tags', async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    });
+  });
+  await page.goto('https://conduit.bondaracademy.com/');
+  
+});
+
+
+test('has Title', async ({ page }) => {
+  
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+})
